f61d254e92d227300888369f56b3b4e34fbd9156
Anirudh Madabhushi <anirudhroxrulz@gmail.com>
2016-10-20 23:19:27 -0700

    Posting to the RuutDrop contactus API

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "http://ruutdrop-api.appspot.com/v1/contactus",
                 type: "POST",
                 data: {
                     name: name,
